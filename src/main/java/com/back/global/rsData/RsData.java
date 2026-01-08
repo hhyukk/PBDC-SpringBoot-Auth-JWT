8027febbba5131f94c6a6f0870380b87dea55ff9
hhyukk <hhyukk1273@gmail.com>
2026-01-08 15:17:17 +0900

    refactor: 공통 응답 객체 RsData를 범용 타입으로 재정의

--- src/main/java/com/back/global/rsData/RsData.java
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -1,6 +1,4 @@
 package com.back.global.rsData;
 
-import com.back.domain.post.postComment.dto.PostCommentDto;
-
-public record RsData(String resultCode, String ms, PostCommentDto data) {
+public record RsData(String resultCode, String ms, Object data) {
 }
