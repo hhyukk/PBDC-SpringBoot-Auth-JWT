805fa86d409d912eeacb1d44fc9973c90c7608b7
hhyukk <hhyukk1273@gmail.com>
2026-01-08 10:59:04 +0900

    refactor: @RestController 적용에 따라 @ResponseBody 중복 제거

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -6,10 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController // @Controller + @ResponseBody
 @RequiredArgsConstructor
 public class ApiV1PostController {
     private final PostService postService;
