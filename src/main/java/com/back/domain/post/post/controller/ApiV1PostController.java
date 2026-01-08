a0656c818d151f07762e161ad061e49a9cc2eaab
hhyukk <hhyukk1273@gmail.com>
2026-01-08 11:24:52 +0900

    feat: 게시글 단건 조회 API 추가

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -4,10 +4,7 @@ import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +15,15 @@ public class ApiV1PostController {
     private final PostService postService;
 
     @GetMapping
-    public List<Post>getItems(){
+    public List<Post> getItems() {
         List<Post> items = postService.findAll();
         return items;
+    }
+
+    @GetMapping("/{id}")
+    public Post getItem(@PathVariable int id) {
+        Post post = postService.findById(id).get();
+
+        return post;
     }
 }
