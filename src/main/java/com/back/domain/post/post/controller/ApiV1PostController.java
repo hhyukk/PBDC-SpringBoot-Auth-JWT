abbf100d7d553b317dcbe3163fd74a8c204d1d74
hhyukk <hhyukk1273@gmail.com>
2026-01-08 12:49:14 +0900

    refactor: 조회 API에 readOnly 트랜잭션 적용

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -4,6 +4,7 @@ import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class ApiV1PostController {
     private final PostService postService;
 
     @GetMapping
+    @Transactional(readOnly = true)
     public List<PostDto> getItems() {
         List<Post> items = postService.findAll();
         return items
@@ -24,6 +26,7 @@ public class ApiV1PostController {
     }
 
     @GetMapping("/{id}")
+    @Transactional(readOnly = true)
     public PostDto getItem(@PathVariable int id) {
         Post post = postService.findById(id).get();
 
