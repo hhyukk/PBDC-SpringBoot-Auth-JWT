60d21e61890262ddd9fb720b5ad2221f41153fe0
hhyukk <hhyukk1273@gmail.com>
2026-01-12 11:18:08 +0900

    test: 게시글 생성 API 응답 JSON 필드 검증 추가

--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -1,5 +1,7 @@
 package com.back.domain.post.post.controller;
 
+import com.back.domain.post.post.entity.Post;
+import com.back.domain.post.post.service.PostService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ActiveProfiles("test") // 테스트 환경에서는 test 프로파일을 활성화
 @SpringBootTest // 스프링부트 테스트 클래스임을 나타냄
@@ -24,6 +25,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class ApiV1PostControllerTest {
     @Autowired
     private MockMvc mvc; // MockMvc를 주입
+    @Autowired
+    private PostService postService;
 
     //회원가입 테스트
     @Test
@@ -41,11 +44,19 @@ public class ApiV1PostControllerTest {
                                         }
                                         """)
                 )
-                .andDo(print());
+                .andDo(print()); // 응답 결과 출력
+
+        Post post = postService.findLatest().get();
+        long totalCount = postService.count();
+        // 201 Created 상태코드 검증
         resultActions
                 .andExpect(handler().handlerType(ApiV1PostController.class))
                 .andExpect(handler().methodName("write"))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.resultCode").value("201-1"))
+                .andExpect(jsonPath("$.msg").value("%d번 글이 작성되었습니다.".formatted(post.getId())))
+                .andExpect(jsonPath("$.data.totalCount").value(totalCount))
+                .andExpect(jsonPath("$.data.post.id").value(post.getId()));
 
     }
 
