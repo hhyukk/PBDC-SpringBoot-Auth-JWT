d0635b091d1a6e713ba573a2ec5f4d43cb3d0251
hhyukk <hhyukk1273@gmail.com>
2026-02-20 15:43:05 +0900

    feat: 회원 가입, 로그인 및 로그아웃 API 구현

--- src/main/java/com/back/domain/member/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -4,9 +4,9 @@ import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.repository.MemberRepository;
 import com.back.global.exception.ServiceException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -16,6 +16,7 @@ import java.util.Optional;
 public class MemberService {
     private final AuthTokenService authTokenService;
     private final MemberRepository memberRepository;
+    private PasswordEncoder passwordEncoder;
 
     public long count() {
         return memberRepository.count();
@@ -26,6 +27,7 @@ public class MemberService {
                 .ifPresent(_member -> {
                     throw new ServiceException("409-1", "이미 존재하는 아이디입니다.");
                 });
+        password = passwordEncoder.encode(password);
 
         Member member = new Member(username, password, nickname);
 
@@ -54,5 +56,11 @@ public class MemberService {
 
     public List<Member> findAll() {
         return memberRepository.findAll();
+    }
+
+    public void checkPassword(Member member, String password) {
+        if (!passwordEncoder.matches(password, member.getPassword())) {
+            throw new ServiceException("401-1", "비밀번호가 일치하지 않습니다.");
+        }
     }
 }
